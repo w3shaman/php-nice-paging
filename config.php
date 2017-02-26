ee9bb33606684740162766526b32b492effbaf5e
smarty24 <smartiano24@yahoo.com>
2017-02-27 00:54:40 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,41 @@
 <?php
-$host="localhost";
-$user="";
-$password="";
-$database="db";
-?>
+
+Class Config {
+
+private static $_dbType = 'mysql';
+private static $_host = '127.0.0.1';
+private static $_username = 'root';
+private static $_password = '';
+private static $_dbname = 'db';
+
+private function __construct() {
+	$this->_dbType = $_dbType;
+	$this->_host = $_host;
+	$this->_username = $_username;
+	$this->_password = $_password;
+	$this->_dname = $_dname;
+
+}
+
+public static function getDbType() {
+	return self::$_dbType;
+}
+
+public static function getHost() {
+	return self::$_host;
+}
+
+public static function getUser() {
+	return self::$_username;
+}
+
+public static function getPass() {
+	return self::$_password;
+}
+
+public static function getDbname() {
+	return self::$_dbname;
+}
+	
+
+} //end of class config

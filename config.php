2449f43a0b52ae3b2bb92106b3ee943d6f187c69
smarty24 <smartiano24@yahoo.com>
2017-03-10 15:03:21 +0100

    Recovered version from today

--- config.php
+++ config.php
@@ -6,5 +6,5 @@ $password = "";
 // Connect to database
 $pdo=new PDO("mysql:host=localhost;dbname=db;", $username, $password);
 
-//i.e orcl or whatever you named your service during installation
+
 ?>

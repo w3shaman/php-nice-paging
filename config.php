664d0b0eff03c86c1c68eba66c20eb90c8b0fd01
w3shaman <bogeyman2007@gmail.com>
2017-03-19 14:08:27 +0700

    Tidying the config file.

--- config.php
+++ config.php
@@ -1,15 +1,17 @@
 <?php
 
-$username = "";
+// Some needed sonnection variables.
+$host = "localhost";
+$username = "root";
 $password = "";
+$database = "db";
 
-// Connect to database
-$pdo=new PDO("mysql:host=localhost;dbname=db;", $username, $password);
+// Connect to database.
+$pdo=new PDO("mysql:host=$host;dbname=$database;", $username, $password);
 
 try {
-		$pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-} catch (PDOException $e) {
+	$pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+}
+catch (PDOException $e) {
 	die("Connection error: ". $e->getMessage());
 }
-
-?>
